50441d37fdcbb6d1935d46a677cbe501c1dd0f1c
MeiaNoiite <bolaknt@gmail.com>
2021-01-24 12:46:38 -0300

    None

--- Commands/Fun/eightball.py
+++ Commands/Fun/eightball.py
@@ -1,6 +1,6 @@
 import discord,random
 from discord.ext import commands
-@commands.command()
+@commands.command(aliases=['8ball'])
 async def eightball(ctx):
     a = ['Não','Sim','Pode ser','Num acredito que seja verdade','Mano?',"pode ser que sim ou pode ser que não"]
     await ctx.reply(random.choice(a))
